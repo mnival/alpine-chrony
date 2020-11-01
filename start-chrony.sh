e63f533880a7d989a9c4df027e89b4fea6c217d8
mnival <1595998+mnival@users.noreply.github.com>
2020-11-01 18:42:50 +0100

    Correct script start-chrony.sh

--- start-chrony.sh
+++ start-chrony.sh
@@ -5,6 +5,6 @@ _CHRONY_CONF="/etc/chrony/chrony.conf"
 
 [ -f ${_CHRONY_PID} ] && rm ${_CHRONY_PID}
 
-env | awk 'BEGIN {FS="="} /chronyconf\./ {gsub(/chronyconf\./, ""); if ($2 != "null") {printf("%s %s\n", $1, $2)}}' > ${_CHRONY_CONF}
+env | awk 'BEGIN {FS="="} /chronyconf\./ {gsub(/chronyconf\./, ""); if ($2 == "") {$2=" "}; if ($2 != "null") {split($2, i, ";"); for(j in i) {printf("%s %s\n", $1, i[j])}}}' > ${_CHRONY_CONF}
 
 exec chronyd -d -s -u chrony
